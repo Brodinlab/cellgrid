f33372f823cffa42d2fcd76e95bfb749d9e88ecf
Yang <yang.chen@ki.se>
2019-07-18 20:00:56 +0200

    Fix version number and url

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/nehcgnay/cellgrid',
-    version='1.0.0',
+    url='https://github.com/Brodinlab/cellgrid',
+    version='0.0.1',
     zip_safe=False,
 )
