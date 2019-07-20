f15be1476218f20121e9b72bb2ba159b482e9d16
Yang <yang.chen@ki.se>
2019-07-20 08:48:11 +0200

    Move markers to const.py

---
+++ cellgrid/const.py
@@ -0,0 +1,9 @@
+DATA_MARKERS = ('CD45', 'HLA-ABC', 'CD57', 'CD19', 'CD5', 'CD16',
+                'CD4', 'CD8a', 'CD11c', 'CD31', 'CD25', 'CD64',
+                'CD123', 'gdTCR', 'CD3e', 'CD7', 'CD26',
+                'CD9', 'CD22', 'CD14', 'CD161', 'CD29', 'HLA-DR',
+                'CD44', 'CD127', 'CD24', 'CD27', 'CD38', 'CD45RA',
+                'CD20', 'CD33', 'IgD', 'CD56', 'CD99', 'CD15', 'CD39',
+                'CD11b')
+
+DNA_BEADS_MARKERS = ('Ce140Di', 'Ir191Di')
