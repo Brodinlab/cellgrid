26d3f6b6fb887d51ab59b184cd54f65850016962
Yang <yang.chen@ki.se>
2019-07-31 16:00:17 +0200

    Clean

--- tests/ensemble/test_classifier.py
+++ tests/ensemble/test_classifier.py
@@ -5,6 +5,8 @@ import pandas as pd
 from cellgrid.ensemble import *
 
 clf = None
+x_train = None
+y_train = None
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -12,10 +14,11 @@ def fit():
     f = os.path.join(os.getcwd(),
                      'tests', 'cellgrid_test.csv')
     df = pd.read_csv(f)
+    global clf, x_train, y_train
+
     y_train = df[['level0', 'level1', 'level2']]
     x_train = df.drop(['level0', 'level1', 'level2'], axis=1)
     schema = GridSchema.from_json()
-    global clf
     clf = GridClassifier(schema)
     clf.fit(x_train, y_train)
 
@@ -28,40 +31,24 @@ class TestGridSchema:
         assert schema.get()[0]['parent'] is None
 
     def test_fit(self):
-        f = os.path.join(os.getcwd(),
-                         'tests', 'cellgrid_test.csv')
-        df = pd.read_csv(f)
-        y_train = df[['level0', 'level1', 'level2']]
-        x_train = df.drop(['level0', 'level1', 'level2'], axis=1)
         r = clf.score(x_train, y_train)
-
         assert list(r.keys()) == ['all-events', 'cells', 'B', 'CD4T',
                                   'CD8T', 'Monocytes', 'NK', 'gdT']
         for v in r.values():
             assert v > 0.9
 
     def test_predict(self):
-        f = os.path.join(os.getcwd(),
-                         'tests', 'cellgrid_test.csv')
-        df = pd.read_csv(f)
-        x_train = df.drop(['level0', 'level1', 'level2'], axis=1)
-
         y = clf.predict(x_train)
         for i in range(3):
             label = 'level{}'.format(i)
-            assert set(y[label].unique()) == set(df[label].unique())
+            assert set(y[label].unique()) == set(y_train[label].unique())
 
     def test_save_and_load(self):
-        f = os.path.join(os.getcwd(),
-                         'tests', 'cellgrid_test.csv')
-        df = pd.read_csv(f)
-        x_train = df.drop(['level0', 'level1', 'level2'], axis=1)
-        y_train = df[['level0', 'level1', 'level2']]
         with tempfile.TemporaryDirectory() as tmpdir:
             f = os.path.join(tmpdir, 'model.txt')
             save_model(clf, f)
             clf2 = load_model(f)
-            r = clf.score(x_train, y_train)
+            r = clf2.score(x_train, y_train)
             assert list(r.keys()) == ['all-events', 'cells', 'B', 'CD4T',
                                       'CD8T', 'Monocytes', 'NK', 'gdT']
             for v in r.values():
